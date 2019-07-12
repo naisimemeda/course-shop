b467e687e8f9a4f1053fde4efd89b52898582fac
vagrant <vagrant@homestead>
2019-07-12 12:45:28 +0000

    加入swoole

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Hhxsv5\LaravelS\Illuminate\LaravelSServiceProvider::class,
 
     ],
 
