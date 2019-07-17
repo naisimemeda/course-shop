656e286c299d2b6741edaaaedc3a40896e999495
naisimemeda <2514430140@qq.com>
2019-07-17 02:50:51 +0000

    商品后台添加类目字段

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,6 +16,8 @@ $factory->define(Product::class, function (Faker $faker) {
         "https://cdn.learnku.com/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
         "https://cdn.learnku.com/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
     ]);
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
+
 
     return [
         'title'        => $faker->word,
@@ -26,5 +28,6 @@ $factory->define(Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        'category_id'  => $category ? $category->id : null,
     ];
 });
